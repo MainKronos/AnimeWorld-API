2e952b0a75fa6cb06b355db74d4c9b1e401d9995
Krónos <lorenzo.chesi@live.it>
2022-02-12 20:23:44 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="animeworld",
-    version="1.4.9",
+    version="1.4.10",
     author="MainKronos",
     description="AnimeWorld UNOFFICIAL API",
     long_description=long_description,
