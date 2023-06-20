9bb3541c71df74003509b25e8de7a463714563ce
git config --global user.name <lorenzo.chesi@live.it>
2023-06-20 18:41:27 +0200

    1.6.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="animeworld",
-    version="1.5.1",
+    version="1.6.0",
     author="MainKronos",
     description="AnimeWorld UNOFFICIAL API",
     long_description=long_description,
