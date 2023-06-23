d82011eb07e99ccfda1c960d426db37fa6d6dc0f
git config --global user.name <lorenzo.chesi@live.it>
2023-06-23 09:48:29 +0200

    up

--- readme.md
+++ readme.md
@@ -1,3 +1,10 @@
+# Swagger
+
+https://mainkronos.github.io/AnimeWorld-API/
+
+
+# DEV
+
 [Editor](https://editor-next.swagger.io/)
 
 https://swagger.io/docs/specification/about/
--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import sys
 import httpx
 from http.server import HTTPServer, BaseHTTPRequestHandler
+from http.cookies import SimpleCookie
 
 # sudo ./main.py 80 http://jenkins.example.com:8080/
 
@@ -10,8 +11,6 @@ URL = "https://www.animeworld.so/api"
 
 class Redirect(BaseHTTPRequestHandler):
 
-    def __init__(self, *args, **kwargs) -> None:
-        super().__init__(*args, **kwargs)
 
     def do_OPTIONS(self):
         self.send_response(200)
@@ -24,36 +23,39 @@ class Redirect(BaseHTTPRequestHandler):
         self.end_headers()
 
     def do_POST(self):
-        content_len = int(self.headers.get('Content-Length'))
-        data = self.rfile.read(content_len)
-
-        with httpx.Client(http2=True) as client:
-            try:
-                res = client.post("https://www.animeworld.so/api" + self.path, data=data.decode(), headers=dict(self.headers))
-
-                self.send_response(res.status_code)
-                self.send_header('Access-Control-Allow-Origin', '*')
-                self.send_header('Content-Type', res.headers['Content-Type'])
-                self.end_headers()
-                self.wfile.write(res.content)
-            except httpx.ReadTimeout:
-                self.send_response(403)
-                self.send_header('Access-Control-Allow-Origin', '*')
-                self.end_headers()
+        self.do_REQUEST()
 
     def do_GET(self):
 
-        with httpx.Client(http2=True) as client:
-            res = client.get("https://www.animeworld.so/api" + self.path, headers=self.headers)
+        self.do_REQUEST()
 
-            self.send_response(res.status_code)
+    def do_REQUEST(self):
+        cookies = dict(SimpleCookie(self.headers.get('Cookie')))
+        headers = {}
+        if self.headers.get('csrf-token'): 
+            headers['csrf-token'] = self.headers.get('csrf-token')
+        
+        data = None
+        content_len = self.headers.get('Content-Length')
+        if content_len:
+            data = self.rfile.read(int(content_len))
+
+        content = None
+        try:
+            with httpx.Client(http2=True) as client:
+                res = client.request(method=self.command, url="https://www.animeworld.so/api" + self.path, headers=headers, cookies=cookies, content=data)
+                content = res.content
+                
+                self.send_response(res.status_code)
+                self.send_header('Content-Type', res.headers['Content-Type'])
+        except httpx.ReadTimeout:
+            self.send_response('403')
+        finally:
             self.send_header('Access-Control-Allow-Origin', '*')
-            self.send_header('Content-Type', res.headers['Content-Type'])
-            # for h in res.headers:
-            #     self.send_header(h, res.headers[h])
             self.end_headers()
-            self.wfile.write(res.content)
 
+            if content: self.wfile.write(content)
+            
 
 if __name__ == "__main__":
     sock = HTTPServer(("", int(8000)), Redirect)
