045f0cda2b5f3eba7c1eabb46a0dda41500d0f9a
Lorenzo Chesi <lorenzo.chesi@live.it>
2025-01-16 22:15:55 +0100

    workflows

--- setup.py
+++ setup.py
@@ -1,24 +1,6 @@
 import setuptools
-
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
+import os
 
 setuptools.setup(
-    name="animeworld",
-    version="1.6.5",
-    author="MainKronos",
-    description="AnimeWorld UNOFFICIAL API",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://mainkronos.github.io/AnimeWorld-API/",
-    packages=setuptools.find_packages(),
-    install_requires=['httpx', 'httpx[http2]', 'youtube_dl', 'beautifulsoup4'],
-	license='MIT',
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-		"Topic :: Utilities",
-    ],
-    python_requires='>= 3.7',
+    version=os.environ["RELEASE_VERSION"]
 )
